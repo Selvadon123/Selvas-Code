cca6ea26a6b45fb3addf6b553b97123011f19425
Selvadon123 <selvasuki1234@gmail.com>
2022-02-25 22:40:02 +0530

    initial commit

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,13 @@
+import java.util.*
+;
+
+
 public class Helloworld
 {
     public static void main(String [] args)
     {
+        Scanner scn = new Scanner(System.in);
+        
         System.out.println("Helloworld");
     }
 }
